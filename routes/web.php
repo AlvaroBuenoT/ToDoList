5e790db8cba651596d341389593785da664b8d6b
Alvaro <202012663j@utea.edu.pe>
2024-08-10 16:46:43 -0500

    Quinto Comit

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', [TaskController::class,'index']);
-Route::post('/', [TaskController::class,'store']);
-Route::delete('/{id}', [TaskController::class,'destroy'])->name('task.destroy');
+// Rutas de autenticación
+Auth::routes();
+
+// Rutas protegidas por autenticación
+Route::middleware('auth')->group(function () {
+    Route::get('/', [TaskController::class, 'index'])->name('home');
+    Route::post('/', [TaskController::class, 'store']);
+    Route::delete('/{id}', [TaskController::class, 'destroy'])->name('task.destroy');
+});
